1b26b4ec083a16c8af0d10ed0d9f9511ab213b17
microgreeney <microgreeney@gmail.com>
2026-04-20 03:56:03 +0800

    Update firebase-auth.js

--- firebase-auth.js
+++ firebase-auth.js
@@ -4,6 +4,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   sendPasswordResetEmail,
+  sendEmailVerification,
   onAuthStateChanged,
   signOut,
   GoogleAuthProvider,
